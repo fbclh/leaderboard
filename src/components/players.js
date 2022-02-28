ce817ac7b753726cb432e4bd5a7c5909d7409b41
fbclh <fbclh@protonmail.com>
2022-02-28 18:52:43 -0300

    Add JS

---
+++ src/components/players.js
@@ -0,0 +1,48 @@
+const listPlayers = document.querySelector('.recent-score__players');
+
+class Players {
+  constructor(name, score) {
+    this.name = name;
+    this.score = score;
+    this.players = [];
+  }
+
+  populateFields() {
+    localStorage.setItem('savedPlayers', JSON.stringify(this.players));
+  }
+
+  removePlayer(player) {
+    this.players = this.players.filter((item) => item !== player);
+    this.populateFields;
+  }
+
+  addPlayer(newPlayer) {
+    this.players.push(newPlayer);
+    this.populateFields;
+    this.displayPlayers;
+  }
+
+  displayPlayers() {
+    listPlayers.innerHTML = '';
+    if (this.players.length > 0) {
+      this.players.map((player) => {
+        const divPlayer = document.createElement('tr');
+        const elementPlayer = document.createElement('td');
+        elementPlayer.textContent = `${player.name}: ${player.score}`;
+        divPlayer.classList.add('.recent-score__container');
+        divPlayer.appendChild(elementPlayer);
+        listPlayers.appendChild(divPlayer);
+        return listPlayers;
+      });
+    } else {
+      const divPlayer = document.createElement('tr');
+      const elementPlayer = document.createElement('td');
+      elementPlayer.textContent = 'Add new player';
+      divPlayer.classList.add('.recent-score__container');
+      divPlayer.appendChild(elementPlayer);
+      listPlayers.appendChild(divPlayer);
+    }
+  }
+}
+
+export default Players;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import Players from './components/players.js';
+import './style.css';
+
+const form = document.querySelector('.add-score__form');
+const [name, score] = form.elements;
+const objPlayers = new Players();
+
+if (localStorage.savedPlayers) {
+  objPlayers.players = JSON.parse(localStorage.getItem('savedPlayers'));
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault;
+  const newPlayer = {
+    name: name.value,
+    score: score.value,
+  };
+
+  objPlayers.addPlayer(newPlayer);
+  name.value = '';
+  score.value = '';
+});
+
+objPlayers.displayPlayers();
+objPlayers.populateFields();
